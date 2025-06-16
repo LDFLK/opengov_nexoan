b27d815e819a46fb5f79e994610fe6da771d9363
Vibhatha Abeykoon <vibhatha@gmail.com>
2025-06-16 14:22:54 +0530

    fix: adding document rns

--- scripts/insert_sample.sh
+++ scripts/insert_sample.sh
@@ -1,2 +1,4 @@
 ./orgchart -data $(pwd)/data/orgchart/akd/2024-09-27 -init true
 ./orgchart -data $(pwd)/data/people/akd/2024-09-25/ -type person
+
+./orgchart -data $(pwd)/data/documents/akd/organisation/ -type document
