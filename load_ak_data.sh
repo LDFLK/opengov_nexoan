063ad28ddf6a458b56dc44ee0bd0615c545db738
zaeema-n <zaeemanashath@gmail.com>
2025-06-18 06:51:56 +0530

    Added Anura's first cabinet release data

--- load_ak_data.sh
+++ load_ak_data.sh
@@ -3,3 +3,6 @@
 # Load Anura's presidency data (termination of Ranil's presidency and starting Anura's)
 ./orgchart -data $(pwd)/data/people/akd/2024-09-23/ -type person
 ./orgchart -data $(pwd)/data/orgchart/akd/2024-09-23/
+
+# Load Anura's first cabinet release
+./orgchart -data $(pwd)/data/orgchart/akd/2024-09-27/
