0483438625145a8a98faeb4067362fcc4d45f055
zaeema-n <zaeemanashath@gmail.com>
2025-08-30 16:06:08 +0530

    Added a portion of Gotabaya's orgchart data

---
+++ load_gr_data.sh
@@ -0,0 +1,13 @@
+# Load Gota's presidency data
+./orgchart -data "$(pwd)/data/people/Gotabaya Rajapaksa/2019-11-17/" -init -type person
+
+# Load Gota's org data
+./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2019-12-10/"
+./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2019-12-31/"
+# ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2019-01-09/"
+# ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-13/"
+# ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-22/2159_15/"
+# ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-22/2159_21/"
+# ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-24/"
+
+
